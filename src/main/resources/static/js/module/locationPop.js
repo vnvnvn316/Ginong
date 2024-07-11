903542fa3b5ad2ececaf3c6b997fd8e1e1decc79
minkyung16 <puppe316@naver.com>
2024-07-11 20:08:50 +0900

    fix: 배송지 팝업창 선택시 닫기
    [자식팝업창에서 부모팝업창으로 id값 전송]

--- src/main/resources/static/js/module/locationPop.js
+++ src/main/resources/static/js/module/locationPop.js
@@ -2,18 +2,18 @@
 const locationSection = document.querySelector(".location-info");
 const changeBtn = locationSection.querySelector(".n-btn");
 
-//변경버튼을 클릭했을 때 location-pop.html 자식창 띄우기
-changeBtn.onclick = function (){
+    //변경버튼을 클릭했을 때 location-pop.html 자식창 띄우기
+    changeBtn.onclick = function (){
 
-    const selectedId = locationSection.querySelector(".location-id").value;
-    const popup = window.open("/location-pop", "_blank", "width=600,height=200, top=250,left=100");
+        const selectedId = locationSection.querySelector(".location-id").value;
+        const popup = window.open("/location-pop", "_blank", "width=600,height=200, top=250,left=100");
 
-    // 자식 창이 로드된 후에 메시지를 보냄
-    popup.onload = function() {
-        popup.postMessage(selectedId, "*");
-    };
+        // 자식 창이 로드된 후에 메시지를 보냄
+        popup.onload = function() {
+            popup.postMessage(selectedId, "*");
+        };
 
-}
+    }
 
 //--------------------------------------------------------
 
